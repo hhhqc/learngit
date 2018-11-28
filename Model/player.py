70fa896d7d0f7ae6f6002abc7b21ec83a994853b
hhhqc <609853435@qq.com>
2018-11-28 19:01:41 +0800

    updata

--- Model/player.py
+++ Model/player.py
@@ -1,7 +1,7 @@
 
 
 class Player(object):
-    def __init__(self, name, blood, strength, agile, experience, level):
+    def __init__(self, name, blood,mana,strength, agile,intelligence,physique,criticalChance,defenses, experience, level):
         self.name = name
         self.blood = blood
         self.strength = strength
@@ -9,6 +9,7 @@ class Player(object):
         self.experience = experience
         self.level = level
         self.attack = self.strength
+
     # 名字
     @property
     def name(self):
@@ -20,21 +21,35 @@ class Player(object):
     # @name.deleter
     # def name(self):
     #     del self._name
-    # 血量
 
+    # 血量
     @property
     def blood(self):
         return self._blood
-
+    #血量=100+体质*5
+    @blood.setter
+    def blood(self):
+        self._blood = 100 + self.physique * 5
     @blood.setter
     def blood(self, value):
-        self._blood = value
-    # 攻击
+        self._blood = 100 + self.physique * 5 + value
 
+    # 法力
+    @property
+    def mana(self):
+        return self._mana
+    #法力=100+智力*5
+    @mana.setter
+    def mana(self):
+        self._mana = 100 +  self._intelligence * 5
+    @mana.setter
+    def mana(self,value):
+        self._mana = 100 + self._intelligence * 5 + value
+
+    # 攻击
     @property
     def attack(self):
         return self._attack
-
     @attack.setter
     def attack(self, value):
         self._attack = 10 + value * 2
@@ -53,7 +68,6 @@ class Player(object):
     @property
     def strength(self):
         return self._strength
-
     @strength.setter
     def strength(self, value):
         self._strength = value
@@ -62,15 +76,54 @@ class Player(object):
     @property
     def agile(self):
         return self._agile
-
     @agile.setter
     def agile(self, value):
         self._agile = value
-    # 速度
 
+    # 智力
+    @property
+    def intelligence(self):
+        return self._intelligence
+    @intelligence.setter
+    def intelligence(self,value):
+        self._intelligence = value
+
+    #体质
+    @property
+    def physique(self):
+        return self._physique
+    @physique.setter
+    def physique(self,value):
+        self._physique = value
+
+    #暴击率
+    @property
+    def criticalChance(self):
+        return self._criticalChance
+    @criticalChance.setter
+    def criticalChance(self,value):
+        self._criticalChance = value
+
+    #技能伤害倍率
+    @property
+    def skillInjuryRate(self):
+        return self._skillInjuryRate
+
+    #防御力
+    @property
+    def defenses(self):
+        return self._defenses
+    @defenses.setter
+    def defenses(self,value):
+        self._defenses = value
+
+    # 速度
     @property
     def speed(self):
-        return 10 + self._agile * 2
+        return self._speed
+    @speed.setter
+    def speed(self,value):
+        self._speed = 10 + self._agile * 1.5
 
     # 经验
     @property
