91a3fdfe025ce801b67750d00474735155eab231
hhhqc <609853435@qq.com>
2018-11-30 18:54:47 +0800

    人物

--- Model/player.py
+++ Model/player.py
@@ -1,14 +1,20 @@
 
 
 class Player(object):
-    def __init__(self, name, blood,mana,strength, agile,intelligence,physique,criticalChance,defenses, experience, level):
+    def __init__(self, name, blood,attack,mana,strength, agile,intelligence,physique,criticalChance,skillInjuryRate,defenses, experience, level):
         self.name = name
         self.blood = blood
+        self.mana = mana
+        self.attack = attack
         self.strength = strength
         self.agile = agile
+        self.intelligence = intelligence
+        self.physique = physique
+        self.criticalChance = criticalChance
+        self.skillInjuryRate = skillInjuryRate
+        self.defenses = defenses
         self.experience = experience
         self.level = level
-        self.attack = self.strength
 
     # 名字
     @property
@@ -22,11 +28,10 @@ class Player(object):
     # def name(self):
     #     del self._name
 
-    # 血量
+    # 血量=100+体质*5+装备血量
     @property
     def blood(self):
         return self._blood
-    #血量=100+体质*5
     @blood.setter
     def blood(self):
         self._blood = 100 + self.physique * 5
@@ -34,11 +39,10 @@ class Player(object):
     def blood(self, value):
         self._blood = 100 + self.physique * 5 + value
 
-    # 法力
+    # 法力=100+智力*5+装备法力
     @property
     def mana(self):
         return self._mana
-    #法力=100+智力*5
     @mana.setter
     def mana(self):
         self._mana = 100 +  self._intelligence * 5
@@ -46,25 +50,25 @@ class Player(object):
     def mana(self,value):
         self._mana = 100 + self._intelligence * 5 + value
 
-    # 攻击
+    # 攻击=10+力量*2
     @property
     def attack(self):
         return self._attack
     @attack.setter
     def attack(self, value):
-        self._attack = 10 + value * 2
+        self._attack = 10 + self._strength * 2 + value
 
-    # 最大攻击
+    # 最大攻击=攻击*1.1
     @property
     def attackMax(self):
         return self._attack * 1.1
 
-    # 最小攻击
+    # 最小攻击=攻击*0.9
     @property
     def attackMin(self):
         return self._attack * 0.9
 
-    # 力量
+    # 力量=力量+装备力量
     @property
     def strength(self):
         return self._strength
@@ -72,7 +76,7 @@ class Player(object):
     def strength(self, value):
         self._strength = self._strength + value
 
-    # 敏捷
+    # 敏捷=敏捷+装备敏捷
     @property
     def agile(self):
         return self._agile
@@ -80,50 +84,53 @@ class Player(object):
     def agile(self, value):
         self._agile = self._agile + value
 
-    # 智力
+    # 智力=智力+装备智力
     @property
     def intelligence(self):
         return self._intelligence
     @intelligence.setter
     def intelligence(self,value):
-        self._intelligence = value
+        self._intelligence = self._intelligence + value
 
-    #体质
+    #体质=体质+装备体质
     @property
     def physique(self):
         return self._physique
     @physique.setter
     def physique(self,value):
-        self._physique = value
+        self._physique = self._physique + value
 
-    #暴击率
+    #暴击率=(10+敏捷*0.3)
     @property
     def criticalChance(self):
         return self._criticalChance
     @criticalChance.setter
+    def criticalChance(self):
+        self._criticalChance = (10+self._agile*0.3)
+    @criticalChance.setter
     def criticalChance(self,value):
-        self._criticalChance = value
+        self._criticalChance = self._criticalChance + value
 
-    #技能伤害倍率
+    #技能伤害倍率=(1+智力*0.02)
     @property
     def skillInjuryRate(self):
-        return self._skillInjuryRate
+        return 1+self._intelligence*0.02
 
-    #防御力
+    #防御力=10+体质*1+装备防御
     @property
     def defenses(self):
         return self._defenses
     @defenses.setter
     def defenses(self,value):
-        self._defenses = value
+        self._defenses = 10+ self._physique*1 + value
 
-    # 速度
+    # 速度=10+敏捷*1.5+装备敏捷
     @property
     def speed(self):
         return self._speed
     @speed.setter
     def speed(self,value):
-        self._speed = 10 + self._agile * 1.5
+        self._speed = 10 + self._agile * 1.5 + value
 
     # 经验
     @property
@@ -132,7 +139,7 @@ class Player(object):
 
     @experience.setter
     def experience(self, value):
-        self._experience = value
+        self._experience =  self._experience + value
 
     # 等级
     @property
@@ -141,4 +148,4 @@ class Player(object):
 
     @level.setter
     def level(self, value):
-        self._level = value
+        self._level = self._level + value
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,17 +1,36 @@
 from CombatSystem.combat import Combat
 from Model.player import Player
 from Model.npc import NPC
+from config import *
+import os
+import configparser
 
 
 def main():
-    print("战斗开始")
-    player = Player('player',100,20,10,0,1) # 名字,血量,力量,敏捷,经验,等级
-    players = [player]
-    npc = NPC('npc',100,10,10,0,1)
-    npcs = [npc]
+    # print("战斗开始")
+    # player = Player('player',100,20,10,0,1) # 名字,血量,力量,敏捷,经验,等级
+    # players = [player]
+    # npc = NPC('npc',100,10,10,0,1)
+    # npcs = [npc]
+    # combatSystem = Combat
+    # combatSystem.Combat(players, npcs)
+    conf = configparser.ConfigParser()
+    conf.read(r"..\config\player.config")
+    print(conf.getint('player', 'player_level'))
+    player = Player(conf.get('player', 'player_name'),
+                    conf.getfloat('player', 'player_blood'),
+                    conf.getfloat('player', 'player_mana'),
+                    conf.getfloat('player', 'player_attack'),
+                    conf.getint('player', 'player_strength'),
+                    conf.getint('player', 'player_agile'),
+                    conf.getint('player', 'player_intelligence'),
+                    conf.getint('player', 'player_physique'),
+                    conf.getfloat('player', 'player_criticalChance'),
+                    conf.getfloat('player', 'player_skillInjuryRate'),
+                    conf.getfloat('player', 'player_defenses'),
+                    conf.getfloat('player', 'player_experience'),
+                    conf.getint('player', 'player_level'))
 
-    combatSystem = Combat
-    combatSystem.Combat(players, npcs)
 
 
 if __name__ == "__main__":
